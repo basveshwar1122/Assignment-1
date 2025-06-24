608d0e71a094b220fef5ea7f791023cd26f8cd57
basveshwar1122 <167511434+basveshwar1122@users.noreply.github.com>
2025-06-24 12:51:33 +0530

    Add files via upload

---
+++ Assignment no 1.py
@@ -0,0 +1,23 @@
+a=int(input("Enter  first number: "))
+b=int(input("enter second number: "))
+
+
+c=a+b
+print("addition: ",c)
+
+d=a-b
+print("substraction: ",d)
+
+e=a*b
+print("multiplecation: ",e)
+
+f=a/b
+print("division: ",f)
+
+
+
+
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+full_name = first_name + " " + last_name
+print("Hello, ",full_name,"!", "Welcome to the Python Progra. ")
